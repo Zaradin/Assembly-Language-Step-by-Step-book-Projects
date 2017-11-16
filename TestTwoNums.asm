e65f75c112598895697e88a087cba5c68f4e9e81
romboot <crottyjj@hotmail.com>
2017-11-16 23:00:04 +0000

    Update TestTwoNums.asm

--- TestTwoNums.asm
+++ TestTwoNums.asm
@@ -27,64 +27,64 @@ section .text
 _start:
 
 read1:
-		mov eax,3							; sys_call read
-		mov ebx,0							; file descriptor
+		mov eax,3						; sys_call read
+		mov ebx,0						; file descriptor
 		mov ecx,Num1 						; read into
 		mov edx,NumLen 						; read length
-		int 80h 							; sys_interrupt
+		int 80h 						; sys_interrupt
 
-		cmp eax,-1 							; error handler
+		cmp eax,-1 						; error handler
 		je exitError
 
 read2:
-		mov eax,3							; sys_call read
-		mov ebx,0							; file descriptor
+		mov eax,3						; sys_call read
+		mov ebx,0						; file descriptor
 		mov ecx,Num2 						; read into
 		mov edx,NumLen 						; read length
-		int 80h 	 						; sys_interrupt
+		int 80h 	 					; sys_interrupt
 
-		cmp eax,-1 							; error handler
+		cmp eax,-1 						; error handler
 		je exitError
 
 check:
 		mov ah,byte [Num1]
 		mov al,byte [Num2]
 		cmp ah,al
-		jg writeG 							; jump if greater
-		jl writeL 							; jump if less
-		je writeE 							; jump if equal
+		jg writeG 						; jump if greater
+		jl writeL 						; jump if less
+		je writeE 						; jump if equal
 		jmp exitError 						; something went wrong jump exitError
 
 writeG:
-		mov eax,4 							; sys_call write
-		mov ebx,1 							; file descriptor 1: standard output
-		mov ecx,GreaterThan 				; memory address, start of GreaterThan
+		mov eax,4 						; sys_call write
+		mov ebx,1 						; file descriptor 1: standard output
+		mov ecx,GreaterThan 					; memory address, start of GreaterThan
 		mov edx,GreaterLen 					; GreaterLen
-		int 80h 							; sys_interrupt
+		int 80h 						; sys_interrupt
 		jmp done
 
 writeL:
-		mov eax,4 							; sys_call write
-		mov ebx,1 							; file descriptor 1: standard output
+		mov eax,4 						; sys_call write
+		mov ebx,1 						; file descriptor 1: standard output
 		mov ecx,LessThan 					; memory address, start of LessThan
 		mov edx,LessLen 					; LessLen
-		int 80h 							; sys_interrupt
+		int 80h 						; sys_interrupt
 		jmp done
 
 writeE:
-		mov eax,4 							; sys_call write
-		mov ebx,1 							; file descriptor 1: standard output
+		mov eax,4 						; sys_call write
+		mov ebx,1 						; file descriptor 1: standard output
 		mov ecx,Equal 						; memory address, start of Equal
 		mov edx,EqualLen 					; EqualLen
-		int 80h 							; sys_interrupt
+		int 80h 						; sys_interrupt
 		jmp done
 
 done:
-		mov eax,1 							; sys_call exit
-		mov ebx,0 							; exit status 1
-		int 80h 							; sys_interrupt
+		mov eax,1 						; sys_call exit
+		mov ebx,0 						; exit status 1
+		int 80h 						; sys_interrupt
 
 exitError:
-		mov eax,1 							; sys_call exit
-		mov ebx,-1 							; exit status -1
-		int 80h  							; sys_interrupt
+		mov eax,1 						; sys_call exit
+		mov ebx,-1 						; exit status -1
+		int 80h  						; sys_interrupt
